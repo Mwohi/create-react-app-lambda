1876769865dfe265877e95d55948c5f24a673483
mohadeseh <mohadesehmirzaee2005@gmail.com>
2022-10-13 12:10:10 +0200

    HelloComponent

--- src/App.js
+++ src/App.js
@@ -31,6 +31,44 @@ class LambdaDemo extends Component {
   }
 }
 
+
+class BetterHelloComponent extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { greeting: "Hello" }
+  }
+
+  handleClick = newGreeting => e => {
+    e.preventDefault()
+
+    this.setState({ greeting: newGreeting })
+  }
+
+  render() {
+
+    return (
+      <p>
+        <button onClick={this.handleClick("Hallo")}>Deutsch</button>
+        <button onClick={this.handleClick("salam")}>Persisch</button>
+        <br />
+        <span>{ this.state.greeting} {this.props.name}</span>
+      </p>
+    )
+  }
+}
+
+
+function HelloComponent(props) {
+ 
+  let greeting = "Hello"
+
+  return (
+    <p>
+      {greeting} {props.name}!!! 
+    </p>
+  )
+}
+
 class App extends Component {
   render() {
     return (
@@ -40,7 +78,11 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <LambdaDemo />
+          <HelloComponent name="Mohadeseh" />
+          <HelloComponent name="Christoph" />
+          
+          <BetterHelloComponent name="Mohadeseh" />
+
         </header>
       </div>
     )
